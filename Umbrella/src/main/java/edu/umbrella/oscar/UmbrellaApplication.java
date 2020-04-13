8e0eacd777a0d8cd3cc317ac469de80173364fd2
gmata-ias <gmata@integralads.com>
2020-04-13 17:30:37 -0500

    Initial Commit, Workspace setup and MYSQL. Close #1

---
+++ Umbrella/src/main/java/edu/umbrella/oscar/UmbrellaApplication.java
@@ -0,0 +1,13 @@
+package edu.umbrella.oscar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellaApplication.class, args);
+	}
+
+}
---
+++ Umbrella/src/test/java/edu/umbrella/oscar/UmbrellaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.umbrella.oscar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
